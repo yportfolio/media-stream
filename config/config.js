d74a7fae589bc2360921fe119532b0efb991b7d5
wangyes <wangyes@yonyou.com>
2021-08-08 19:33:59 +0800

    first commit

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const config = {
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri:
+    process.env.MONGODB_URI ||
+    process.env.MONGO_HOST ||
+    "mongodb+srv://marketplace:wy6524615@cluster0.oe2ip.mongodb.net/mern-marketplace?retryWrites=true&w=majority",
+};
+
+export default config;
